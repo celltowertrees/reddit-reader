70e9f71eaa47f42873e4e6f38f3696c7cdd624f6
Kelly Monson <monson.exe@gmail.com>
2017-12-14 12:29:21 -0500

    more css

--- src/components/item.js
+++ src/components/item.js
@@ -3,22 +3,30 @@ import React from 'react';
 const Item = (props) => {
   const permalink = `http://reddit.com/${props.post.data.permalink}`;
   const item = props.post.data;
-  const hasImage = item.thumbnail && item.thumbnail !== 'default' && item.thumbnail !== 'nsfw' && item.thumbnail !== 'image';
+  const hasImage = item.thumbnail && item.thumbnail !== 'default' && item.thumbnail !== 'nsfw' && item.thumbnail !== 'image' && item.thumbnail !== 'self';
 
   console.log(item);
 
   return (
     <div className="item">
-      <div className="item__inner">
-        <a href={permalink} target="_blank">{item.title}</a>
-        { hasImage &&
-          <img src={item.thumbnail} width={item.thumbnail_width} height={item.thumbnail_height} />
-        }
-        <span>{item.link_flair_text}</span>
-        <span>({item.domain})</span>
-        <span>By {item.author}</span>
-        to <span>{item.subreddit_name_prefixed}</span>
-      </div>
+      <a href={permalink} target="_blank">
+        <div className="item__inner">
+          { item.link_flair_text && 
+            <span className="item__flair">{item.link_flair_text}</span>
+          }
+          <span className="item__subreddit">{item.subreddit_name_prefixed}</span>
+          { hasImage &&
+            <div className="item__thumbnail">
+              <img src={item.thumbnail} width={item.thumbnail_width} height={item.thumbnail_height} />
+            </div>
+          }
+          <h4 className="item__title">{item.title}</h4>
+          <span className="item__meta">
+            <span className="item__domain">({item.domain})</span>
+            <span className="item__author">By {item.author}</span>
+          </span>
+        </div>
+      </a>
     </div>
   )
 }
